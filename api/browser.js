d7489b729536a3e596c69fc3ecf00d7bd4c10790
ibnuputraard <ibnu.putra@nawadata.id>
2023-11-20 17:22:43 +0700

    update express

---
+++ api/browser.js
@@ -0,0 +1,24 @@
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer-core");
+
+async function createBrowser() {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
+    headless: true,
+    ignoreHTTPSErrors: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--single-process",
+    ],
+    ignoreDefaultArgs: ["--disable-extensions"],
+    ignoreHTTPSErrors: true,
+  });
+
+  return browser;
+}
+
+exports = createBrowser;
--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-const chromium = require("@sparticuz/chromium");
-const puppeteer = require("puppeteer-core");
+const express = require("express");
+const app = express();
 
-async function handler(request, response) {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath:
-      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath),
-    headless: true,
-    ignoreHTTPSErrors: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--single-process",
-    ],
-    ignoreDefaultArgs: ["--disable-extensions"],
-    ignoreHTTPSErrors: true,
-  });
-  const page = await browser.newPage();
-  await page.goto("https://vercel.com/");
-  const title = await page.title();
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
 
-  await page.close();
-
-  await browser.close();
-
-  response.status(200).json({
-    body: request.body,
-    cookies: request.cookies,
-    title,
-    chromium: await chromium.executablePath,
-  });
-}
-
-handler();
+app.listen(3000, () => {
+  console.log("Listen on the port 3000...");
+});
